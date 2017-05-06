02dd4552bbb71d4215420891ed85e5d2b45a8961
birkin <birkin.diana@gmail.com>
2017-05-06 07:36:27 -0400

    sets up logging.

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 This is a fork of the [PyZ3950](https://github.com/asl2/PyZ3950) library with minor local modifications.  See the original [README](https://github.com/asl2/PyZ3950/blob/master/README.txt) for license information.
 
-
-
-
+This logging branch is not really intended to be merged into the master branch. It's purpose is to add lots of logging to understand the nature of the data passed to/from functions.
 
--- test/test1.py
+++ test/test1.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
+
+import logging, os
 from PyZ3950 import zoom
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s",
+    datefmt="%d/%b/%Y %H:%M:%S",
+    filename=os.environ['LOG_PATH'] )
+log = logging.getLogger(__name__)
+log.debug( 'logging ready' )
+
+
 conn = zoom.Connection ('z3950.loc.gov', 7090)
+log.debug( 'conn type, `{typ}`; conn, ```{val}```'.format( typ=type(conn), val=conn ) )
 conn.databaseName = 'VOYAGER'
 conn.preferredRecordSyntax = 'USMARC'
 
 query = zoom.Query ('CCL', 'ti="1066 and all that"')
+log.debug( 'query type, `{typ}`; query, ```{val}```'.format( typ=type(query), val=query ) )
 
 res = conn.search (query)
+log.debug( 'res type, `{typ}`; res, ```{val}```'.format( typ=type(res), val=res ) )
+
 for r in res:
+    log.debug( 'r type, `{typ}`; r, ```{val}```'.format( typ=type(r), val=r ) )
     print r
+    break
 conn.close ()
 
